07f55a3d1b795a85c7225e76d7ad78551deda478
jericko8 <dimasarianto68@gmail.com>
2022-10-01 16:55:54 +0800

    ubah hardhat.config.js

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -14,6 +14,13 @@ const localRpcUrl = process.env.REACT_APP_CODESPACES
   : "http://" + (global.window ? window.location.hostname : "localhost") + ":8545";
 
 export const NETWORKS = {
+  condor: {
+    name: "condor",
+    color: "#666666",
+    chainId: 188881,
+    blockExplorer: "https://explorer.condor.system",
+    rpcUrl: "https://testnet.condor.system/rpc",
+  },
   localhost: {
     name: "localhost",
     color: "#666666",
